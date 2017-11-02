3d73f6b8160acaad2deaf1cccb08d6359186c3f9
McKenna <mbykowski0413@gmail.com>
2017-11-02 18:40:01 -0400

    Started script for spawning objects

---
+++ LunaLovesPugs/Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour {
+	public GameObject ObjectsToSpawn;
+	public float SpawnTime;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", SpawnTime, SpawnTime);
+		Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+    Instantiate(ObjectsToSpawn, position, Quaternion.identity);
+	}
+
+	void Spawn () {
+		int SpawnPointIndex = Random.Range (10, 10);
+	}
+}
