a2df9a19103c26a37abe4f7c1af84d634ade147d
DESKTOP-5T4AMVE\shelb <slobb@emich.edu>
2017-11-02 00:26:32 -0400

    added things that were missed in previous commit

---
+++ LunaLovesPugs/Assets/Scripts/HighScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+
+	static public int score = 0;
+
+	void Awake(){
+		if (PlayerPrefs.HasKey ("LunaLovesPugsHighScore")) {
+			score = PlayerPrefs.GetInt ("LunaLovesPugsHighScore");
+		}
+		PlayerPrefs.SetInt ("LunaLovesPugsHighScore", score);
+	}
+
+
+	void Start () {
+
+	}
+
+
+	void Update () {
+		Text gt = this.GetComponent<Text> ();
+		gt.text = "High Score: " + score;
+
+		if (score > PlayerPrefs.GetInt ("LunaLovesPugsHighScore")) {
+			PlayerPrefs.SetInt ("LunaLovesPugsHighScore", score);
+		}
+	}
+}
