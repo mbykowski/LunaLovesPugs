9531f2a6d8ee8e5c7c384f784c7eb5554a1cb453
Shelby <slobb@emich.edu>
2017-11-25 01:05:49 -0500

    luna and pug cannot go left, new jump option uparrow

--- LunaLovesPugs/Assets/Scripts/LunaController.cs
+++ LunaLovesPugs/Assets/Scripts/LunaController.cs
@@ -23,6 +23,11 @@ public class LunaController : MonoBehaviour {
 		{
 			jump = true;
 		}
+		//https://gamedev.stackexchange.com/questions/110686/jump-a-gameobject-when-a-key-is-pressed
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			rb2d.velocity = Vector2.zero;
+			rb2d.AddForce (Vector3.down, ForceMode2D.Impulse);
+		}
 		if (Luna.transform.position.y < -10)
 		{
 			LoadScene("GameOver");
@@ -31,15 +36,25 @@ public class LunaController : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-		transform.position += move * maxSpeed * Time.deltaTime;
-		xPOS = (int)transform.position.x;
-
-		if (jump)
+		//only allows right movement
+		if (Input.GetKey(KeyCode.RightArrow))
 		{
-			rb2d.AddForce(new Vector2(0f, jumpForce));
-			jump = false;
+			var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+			transform.position += move * maxSpeed * Time.deltaTime;
+			xPOS = (int)transform.position.x;
+			if (jump)
+			{
+				rb2d.AddForce(new Vector2(0f, jumpForce));
+				jump = false;
+			}
 		}
+		//makes it so they cannot move Left
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position = transform.position;
+		}
+
+
 	}
 
 	//Method Loads a scene by a given name encasped in quotes
