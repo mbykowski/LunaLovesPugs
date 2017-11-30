0ae89266443ca95bb9fc0c9d77c2bf03534b0e61
Shelby <slobb@emich.edu>
2017-11-30 17:18:28 -0500

    extended levels to appropriate length to then add end item

---
+++ LunaLovesPugs/Assets/Scripts/EndPointController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndPointController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("Luna")) {
+			Debug.Log ("collided with Luna"); //correctly displaying
+			GameObject.Find("Timer").SendMessage("Finish");
+			//Loads Scene Game Over
+			LoadScene("Win");
+		}
+		if (other.gameObject.CompareTag ("GoodPickUp") || other.gameObject.CompareTag ("BadPickUp")) {
+			Destroy (other.gameObject);
+		}
+	}
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
